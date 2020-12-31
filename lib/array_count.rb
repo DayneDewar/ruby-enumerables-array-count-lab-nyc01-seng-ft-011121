037e078ce4e17bf9d7e487caf988df4da6f6a7bb
Dayne Dewar <daynedewar@yahoo.com>
2020-12-31 20:58:03 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,10 +1,11 @@
 def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
   i = 0 
-   array.count do |i|
+   while i < array.count  do |i|
      array[i].is_a? String
-   end
      i += 1
+   end
+     
 return
 end
 
